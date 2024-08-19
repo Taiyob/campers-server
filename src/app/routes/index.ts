be74444d2ed47e9b0e9838ee6f4823d408172544
Taiyob <oli1412001@gmail.com>
2024-08-19 10:07:40 +0600

    commit

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -15,9 +15,6 @@ const moduleRoutes = [
   },
 ];
 
-// router.use('/users', UserRoutes);
-// router.use('/students', StudentRoute);
-
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
 
 export default router;
